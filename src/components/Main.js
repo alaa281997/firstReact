2bfff1fc8616d99ab1f36a6ee4148beebe1827c2
alaa281997 <alaabbas97@gmail.com>
2021-05-22 02:12:35 +0300

    lab4

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import HornedBeasts from './HornedBeasts';
-
+import Form from 'react-bootstrap/Form'
 import data from './data.json';
 
 class Main extends React.Component {
@@ -12,18 +12,50 @@ class Main extends React.Component {
             filterData: data
         }
     }
+    showHorns = (e) => {
+        let arrayFilter = data.filter(ele => {
+            if (ele.horns == e.target.value) {
+                return ele
+            }
+            if (e.target.value == 'All') {
+                return ele
+            }
+        }
+
+
+        )
+        this.setState({
+            filterData: arrayFilter
+        })
+        console.log(arrayFilter);
+    }
+
 
     render() {
         return (
             <>
-                {data.map(item => {
+                <Form >
+                    <Form.Group controlId="exampleForm.SelectCustomHtmlSize">
+                        <Form.Label>Filter By Number Of Horns </Form.Label>
+                        <Form.Control as="select" custom name='Horns' onChange={this.showHorns}>
+                            <option value='All'>All</option>
+                            <option value='1'>1</option>
+                            <option value='2'>2</option>
+                            <option value='3'>3</option>
+                            <option value='100'>Wow</option>
+                        </Form.Control>
+                    </Form.Group>
+                </Form>
+
+               
+                {this.state.filterData.map(item => {
                     return (
                         <HornedBeasts
                             title={item.title}
                             imageUrl={item.image_url}
                             description={item.description}
                             getArr={this.props.getArr}
-                           
+
                         />
                     )
                 })}
