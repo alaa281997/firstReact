cc45a59658bfb14eb67238d7d23faad0e7f74846
alaa281997 <alaabbas97@gmail.com>
2021-05-22 01:17:37 +0300

    lab2

--- src/components/HornedBeasts.js
+++ src/components/HornedBeasts.js
@@ -1,22 +1,38 @@
 import React from 'react';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+
 class HornedBeasts extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            numberOfVotes: 0,
+        }
+    }
+
+    numOfVotes = () => {
+        this.setState({
+            numberOfVotes: this.state.numberOfVotes + 1,
+        })
+    }
+
     render() {
         return (
-            <div>
-                <div>
-                    <h1>UniWhal</h1>
-                    <img className='img' src='https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg'
-                        title="UniWhal" alt="UniWhal" height="500px" width="500px" />
-                    <p>A unicorn and a narwhal nuzzling their horns</p>
-                </div>
-                <div>
-                    <h1>Rhino Family</h1>
-                    <img className='img' src='https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80'
-                        title='Rhino Family' alt='Rhino Family' height="500px" width="500px" />
-                    <p>Mother (or father) rhino with two babies</p>
-                </div>
-            </div>
+            <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={this.props.image_url} alt={this.props.title}/>
+            <Card.Body>
+              <Card.Title>{this.props.title}</Card.Title>
+              <Card.Text>
+                {this.props.description}
+              </Card.Text>
+            </Card.Body>
+            <Card.Text>Number of votes : {this.state.numberOfVotes} </Card.Text>
+            <Button variant="primary"  onClick={this.numOfVotes}>Votes</Button>
+            
+          </Card>
         )
     }
 
