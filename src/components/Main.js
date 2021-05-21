3b943a44ef188bb977befc45247110e4854098a4
alaa281997 <alaabbas97@gmail.com>
2021-05-22 00:48:38 +0300

    lab1

---
+++ src/components/Main.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import HornedBeasts from './HornedBeasts';
+
+
+class Main extends React.Component {
+    render() {
+        return (
+            <div>
+               <HornedBeasts/>
+            </div>
+        )
+    }
+
+    
+}
+
+export default Main;
